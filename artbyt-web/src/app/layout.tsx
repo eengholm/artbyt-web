f603b6eca50a7c1a7f6c0b4cdaa91228aff1554e
Emil Engholm <emil@matictribe.com>
2025-12-23 15:40:02 +0100

    more favicon changes

--- artbyt-web/src/app/layout.tsx
+++ artbyt-web/src/app/layout.tsx
@@ -31,29 +31,11 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href={faviconUrl} />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/favicon/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon/favicon-16x16.png"
-        />
+        <link rel="apple-touch-icon" sizes="180x180" href={faviconUrl} />
+        <link rel="icon" type="image/png" sizes="32x32" href={faviconUrl} />
+        <link rel="icon" type="image/png" sizes="16x16" href={faviconUrl} />
         <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link
-          rel="mask-icon"
-          href="/favicon/safari-pinned-tab.svg"
-          color="#000000"
-        />
+        <link rel="mask-icon" href={faviconUrl} color="#000000" />
         <link rel="shortcut icon" href={faviconUrl} />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta
